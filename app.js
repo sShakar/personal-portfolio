96012f91c0d16dab72f12c19eb7b5a5ec2ff063d
sShakar <shakarkosar01@gmail.com>
2022-06-24 01:35:26 +0300

    added about, skills and contacts page

--- app.js
+++ app.js
@@ -5,23 +5,25 @@ hamburgerButton.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
 
+const menuItems = document.querySelectorAll('.menu-item');
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    menu.classList.add('hidden');
+  });
+});
+
 // Sticky Navbar on Scroll
-window.onscroll = function () {
-  myFunction();
-};
+// window.onscroll = function () {
+//   myFunction();
+// };
 
-var navbar = document.getElementById('navbar');
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById('navbar');
+// var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
-  }
-}
-
-// Toggle Navbar on Click
-const hamburgerIcon = document.querySelector('.hamburger-icon');
-
-hamburgerIcon.addEventListener('click', () => {});
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add('sticky');
+//   } else {
+//     navbar.classList.remove('sticky');
+//   }
+// }
